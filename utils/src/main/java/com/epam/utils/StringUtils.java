5b41c550c0cbc75e97e58b85a62fcde880701d6a
EDyugaeva <ed2408@yandex.ru>
2023-01-07 13:10:21 +0500

    commit

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+         int n =
+    }
+
+    public static void main(String[] args) {
+
     }
 }
