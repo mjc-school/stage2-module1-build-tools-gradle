608d858b651bbd821d2de8df320fca0ebd05b8c1
EDyugaeva <ed2408@yandex.ru>
2023-01-08 13:26:32 +0500

    commit-5

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,16 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (!NumberUtils.isCreatable(str)) {
+            return false;
+        }
+
+        float n = NumberUtils.createFloat(str);
+        return n > 0;
     }
+
+
 }
