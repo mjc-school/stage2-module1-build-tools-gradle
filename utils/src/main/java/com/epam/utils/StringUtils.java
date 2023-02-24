a0ff806743425b8c41c364c0b7d09bdaed556794
elshodjava <126221796+elshodjava@users.noreply.github.com>
2023-02-24 08:57:48 +0500

    Update StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,18 +2,15 @@ package com.epam.utils;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
-import java.text.DecimalFormat;
-import java.text.ParseException;
-
 public class StringUtils {
+    public StringUtils() {
+    }
+
     public static boolean isPositiveNumber(String str) {
-        try {
-            return NumberUtils.toDouble(str) > 0.0;
-        }
-        catch (Exception e){
-            return false;
+        return NumberUtils.isParsable(str) && Double.parseDouble(str) > 0.0;
+    }
 
-
-        }
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("12"));
     }
 }
